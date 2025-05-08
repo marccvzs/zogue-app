867059b951ef3363c7ba1dea3e005e48028d5558
Marc Cavazos <marcavazos19@icloud.com>
2025-05-07 21:31:47 -0500

    shad calendar component fixes for new react-day-picker

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -5,7 +5,7 @@ import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 
 function Calendar({
   className,
@@ -60,11 +60,11 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
+        PreviousMonthButton: ({ className, ...props }) => (
+          <Button className={cn("size-4", className)} {...props}><ChevronLeft /></Button>
         ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
+        NextMonthButton: ({ className, ...props }) => (
+          <Button className={cn("size-4", className)} {...props}><ChevronRight /></Button>
         ),
       }}
       {...props}
